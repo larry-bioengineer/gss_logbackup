877d11fb0066782f02d078857ebc5597753f3a11
Lok Yiu To <lokyiuto.larry@gmail.com>
2020-11-29 23:40:59 +0800

    added file path and modify regular expression

--- main.py
+++ main.py
@@ -3,20 +3,27 @@
 # To extrat file from user chosen directory and upload to a google sheet 
 
 import requests
-from datetime import datetime
+from datetime import datetime, date
 import time
+
+checkoutLogPath = "C:\\Program Files (x86)\\Golden News Enterprises Ltd\\Guest Service Station\\Checkout log\\"
+systemLogPath = "C:\\Program Files (x86)\\Golden News Enterprises Ltd\\Guest Service Station\\log\\"
 
 try: 
 	while True:
+		today = date.today()
+		dayString = today.strftime("%d-%m-%Y")
 
 		# Locate Checkout Data from Directory 
-		results = []	
-		with open('checkout_log_demo.log') as f:
-			lines = f.readlines()
+		results = []
+		fileName = dayString + ' checkout.log'
+		directory = checkoutLogPath + fileName
+
+		with open('checkout log demo.log') as f:
+			lines = f.readlines()[1:]
 			for line in lines:
-				r = line.split(" ")
-				results.append({'Date': ' '.join(r[0:3]), 'Room': r[3], 'Remarks': r[4].rstrip("\r\n")})
-		print(results)
+				r = line.split("	")				
+				results.append({'Date': r[0], 'Room': r[1], 'Remarks': r[2].rstrip("\n")})
 
 		# Upload results to Google Sheet 
 		requests.post(
@@ -27,8 +34,11 @@ try:
 
 		# Locate System Log Data from Directory
 		results = []
+		fileName = dayString + '.log'
+		directory = systemLogPath + fileName
+
 		with open('13-11-2020.log') as f:
-			lines = f.readlines()
+			lines = f.readlines()[1:]
 			for line in lines:
 				r = line.split(" 	 ")
 				if len(r) == 3:
