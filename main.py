f44913792bad825bd138be443ab1db2a4515ca0a
Lok Yiu To <lokyiuto.larry@gmail.com>
2020-11-30 12:18:44 +0800

    add features where only data collected within the update time is uploaded

--- main.py
+++ main.py
@@ -2,56 +2,72 @@
 # Created on: 11/26/2020
 # To extrat file from user chosen directory and upload to a google sheet 
 
+# Libraries
 import requests
 from datetime import datetime, date
 import time
 
+# Parameters
 checkoutLogPath = "C:\\Program Files (x86)\\Golden News Enterprises Ltd\\Guest Service Station\\Checkout log\\"
 systemLogPath = "C:\\Program Files (x86)\\Golden News Enterprises Ltd\\Guest Service Station\\log\\"
+updateTime = 5.0;
 
 try: 
 	while True:
 		today = date.today()
+
 		dayString = today.strftime("%d-%m-%Y")
 
 		# Locate Checkout Data from Directory 
 		results = []
 		fileName = dayString + ' checkout.log'
 		directory = checkoutLogPath + fileName
+		
 
 		with open('checkout log demo.log') as f:
 			lines = f.readlines()[1:]
 			for line in lines:
-				r = line.split("	")				
-				results.append({'Date': r[0], 'Room': r[1], 'Remarks': r[2].rstrip("\n")})
+				r = line.split("	")
+
+				# append data within 5 min 
+				datetimeObject = datetime.strptime(r[0], '%m/%d/%Y %I:%M:%S %p') 
+				timeDiff = (datetime.now() - datetimeObject).total_seconds() / 60.0
+				if timeDiff <= updateTime:
+					results.append({'Date': r[0], 'Room': r[1], 'Remarks': r[2].rstrip("\n")})
 
 		# Upload results to Google Sheet 
-		requests.post(
-			"https://sheet.best/api/sheets/e4d92608-bec0-46f4-b43e-1d558e790c28/tabs/Checkout Log",
-			json = results
-		)
+		if len(results) > 0:
+			requests.post(
+				"https://sheet.best/api/sheets/e4d92608-bec0-46f4-b43e-1d558e790c28/tabs/Checkout Log",
+				json = results
+			)
 
 
 		# Locate System Log Data from Directory
 		results = []
-		fileName = dayString + '.log'
+		fileName = dayString + ' system.log'
 		directory = systemLogPath + fileName
 
 		with open('13-11-2020.log') as f:
 			lines = f.readlines()[1:]
 			for line in lines:
 				r = line.split(" 	 ")
-				if len(r) == 3:
+
+				# append data within 5 min 
+				datetimeObject = datetime.strptime(r[0], '%m/%d/%Y %I:%M:%S %p') 
+				timeDiff = (datetime.now() - datetimeObject).total_seconds() / 60.0
+				if timeDiff <= updateTime:				
 					results.append({'Date': r[0], 'Status': r[1], 'Message': r[2].rstrip("\r\n")})
 
 		# Upload results to Google Sheet 
-		requests.post(
-			"https://sheet.best/api/sheets/e4d92608-bec0-46f4-b43e-1d558e790c28/tabs/System Log",
-			json = results
-		)
+		if len(results) > 0:
+			requests.post(
+				"https://sheet.best/api/sheets/e4d92608-bec0-46f4-b43e-1d558e790c28/tabs/System Log",
+				json = results
+			)
 
 		# Program stops for 5 min then re-run 
-		time.sleep(60*5)
+		time.sleep(60*updateTime)
 
 except KeyboardInterrupt:
 	pass
