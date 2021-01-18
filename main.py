8eb8dcf4933a1adbeb59f37a49c0d4f347506c13
Lok Yiu To <lokyiuto.larry@gmail.com>
2021-01-18 10:14:26 +0800

    updated

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ import config
 from pathlib import Path
 
 # Parameters
-checkoutLogPath = "C:\\Program Files (x86)\\Golden News Enterprises Ltd\\Guest Service Station\\Checkout log\\"
-systemLogPath = "C:\\Program Files (x86)\\Golden News Enterprises Ltd\\Guest Service Station\\log\\"
-updateTime = 5.0; # in minutes 
+programPath = "C:\\Program Files (x86)\\Golden News Enterprises Ltd\\Guest Service Station\\log\\"
+# programPath = "/Users/lokyiuto/Documents/CEPO/HSX/GuestServiceStation/LogBackUp/log/"
+checkoutLogPath = programPath + "Checkout log\\"
+systemLogPath = programPath + "System log\\"
+feedbackLogPath = programPath + "Feedback log\\"
+subcribeLogPath = programPath + "Subscribe log\\"
+updateTime = 60.0; # in minutes 
 
 
 try: 
@@ -21,8 +25,9 @@ try:
 		today = date.today()
 		dayString = today.strftime("%m-%d-%Y")
 		print("Periodic Data Backup for Guest Service Station at " + datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+		print("Current Update Period is " + str(updateTime) + " minutes")
 
-		# Locate Checkout Data from Directory 
+		# Locate Checkout Data from Directory---------------- 
 		results = []
 		fileName = dayString + ' checkout.log'
 		directory = checkoutLogPath + fileName
@@ -37,7 +42,7 @@ try:
 					r = line.split("	")
 
 					# append data within 5 min 
-					datetimeObject = datetime.strptime(r[0], '%m/%d/%Y %I:%M:%S %p') 
+					datetimeObject = datetime.strptime(r[0], '%m-%d-%Y %I:%M:%S %p') 
 					timeDiff = (datetime.now() - datetimeObject).total_seconds() / 60.0
 					if timeDiff <= updateTime:
 						results.append({'Datetime': r[0], 'Room': r[1], 'Remarks': r[2].rstrip("\n")})
@@ -53,7 +58,7 @@ try:
 				)
 			print("CHECKOUT data entry uploaded: " + str(len(results)))	
 
-		# Locate System Log Data from Directory
+		# Locate System Log Data from Directory------------
 		results = []
 		fileName = dayString + ' system.log'
 		directory = systemLogPath + fileName
@@ -65,8 +70,8 @@ try:
 				lines = f.readlines()[1:]
 				for line in lines:
 					r = line.split("	")
-					# append data within 5 min 
-					datetimeObject = datetime.strptime(r[0], '%m/%d/%Y %I:%M:%S %p') 
+					# append data within update time
+					datetimeObject = datetime.strptime(r[0], '%m-%d-%Y %I:%M:%S %p') 
 					timeDiff = (datetime.now() - datetimeObject).total_seconds() / 60.0
 					if timeDiff <= updateTime:				
 						results.append({'Datetime': r[0], 'Status': r[1], 'Message': r[2].rstrip("\n")})
@@ -82,7 +87,68 @@ try:
 				)	
 			print("SYSTEM LOG data entry uploaded: " + str(len(results)))
 
-		# Program stops for X min then re-run 
+					# Locate Subscribe Log Data from Directory------------
+		results = []
+		fileName = 'subscription.log'
+		directory = subcribeLogPath + fileName
+
+		if not Path(directory).is_file():
+			print("no file found for Subscription LOG Data")
+		else:
+			with open(directory) as f:
+				lines = f.readlines()[1:]
+				for line in lines:
+					r = line.split("	")
+					# append data within update time
+					datetimeObject = datetime.strptime(r[0], '%m-%d-%Y %I:%M:%S %p') 
+					timeDiff = (datetime.now() - datetimeObject).total_seconds() / 60.0
+					if timeDiff <= updateTime:				
+						results.append({'Date': r[0], 'Firstname': r[1], 'Lastname': r[2], 'Prefix': r[3],
+							'Email': r[4], 'DiscountSubscription': r[5], 'PromotionSubscription': r[6],
+							'SuggestionSubscription': r[7], 'BlogSubscription': r[8].rstrip("\n")})
+
+			# Upload results to Google Sheet 
+			if len(results) > 0:
+				requests.post(
+					config.googleSheetAPI + "/tabs/Subscription",
+					headers={
+						'X-Api-Key': config.serverAPIKey
+					},
+					json = results
+				)	
+			print("Subscription LOG data entry uploaded: " + str(len(results)))
+
+					# Locate Feedback Log Data from Directory------------
+		results = []
+		fileName = 'feedback.log'
+		directory = feedbackLogPath + fileName
+
+		if not Path(directory).is_file():
+			print("no file found for Feedback LOG Data")
+		else:
+			with open(directory) as f:
+				lines = f.readlines()[1:]
+				for line in lines:
+					r = line.split("	")
+					# append data within update time
+					datetimeObject = datetime.strptime(r[0], '%m-%d-%Y %I:%M:%S %p') 
+					timeDiff = (datetime.now() - datetimeObject).total_seconds() / 60.0
+					if timeDiff <= updateTime:				
+						results.append({'Date': r[0], 'Firstname': r[1], 'Lastname': r[2], 'Prefix': r[3],
+							'Email': r[4], 'FilterType': r[5],'Issue': r[6].rstrip("\n")})
+
+			# Upload results to Google Sheet 
+			if len(results) > 0:
+				requests.post(
+					config.googleSheetAPI + "/tabs/CustomerFeedback",
+					headers={
+						'X-Api-Key': config.serverAPIKey
+					},
+					json = results
+				)	
+			print("Feedback LOG data entry uploaded: " + str(len(results)))
+
+		# Program stops for X min then re-run-------------
 		print("")
 		print("Waiting for next Data Backup...")
 
